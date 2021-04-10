f550c47ea6f14f1f913fd2cb22342a2d2c3237cd
hnl09 <henriquenl09@gmail.com>
2021-04-10 15:29:38 -0300

    Add title section and update navbar

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + '/public')) // Tem que colocar arquivos estáticos como HTML e CSS em uma pasta publica para poder usá-los
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
